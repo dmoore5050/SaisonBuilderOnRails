1fade763b184804b3db231ebae3f92414a58c8cf
dmoore5050 <dustinmoore@gmail.com>
2013-06-22 21:54:02 -0500

    recipes controller - moved several instance vars to before_filters

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,11 +2,16 @@ class RecipesController < ApplicationController
 
   add_crumb 'Recipes', '/recipes'
 
+  before_filter :find_recipe, only: [:show, :edit, :update, :destroy]
+  before_filter :init_recipe, only: [:new, :create]
+  before_filter :collect_recipe_ingredients, only: [:show, :edit]
+  before_filter :new_recipe_ingredient, only: [:new, :update]
+  before_filter :all_ingredients, only: [:new, :update, :edit,]
+  before_filter :all_recipe_ingredients, only: [:new, :update]
+  before_filter :usage_list, only: [:new, :update, :edit]
+
   def new
     add_crumb 'Add Recipe', '/'
-    @recipe = Recipe.new params[:recipe]
-    @recipe_ingredient = RecipeIngredient.new
-    @ingredients = Ingredient.all
   end
 
   def index
@@ -14,24 +19,58 @@ class RecipesController < ApplicationController
   end
 
   def show
-    @recipe = Recipe.find params[:id]
-    add_crumb @recipe.name.capitalize, '/'
-    @recipe_ingredients = @recipe.recipe_ingredients.all
+    add_crumb @recipe.name.titleize, '/'
+  end
+
+  def edit
+
+  end
+
+  def update
+    add_crumb @recipe.name.titleize, '/'
+    @recipe.update_attributes(params[:recipe])
   end
 
   def create
-    @recipe = Recipe.new params[:recipe]
-    @recipe.user = current_user
-    @recipe.save
+    @recipe.update_attributes(user: current_user)
     flash[:notice] = "You have added a recipe!"
     redirect_to recipes_url
   end
 
   def destroy
-    @recipe = Recipe.find params[:id]
     @recipe.destroy
     flash[:notice] = 'Recipe has been destroyed!'
     redirect_to recipes_url
+  end
+
+  private
+
+  def find_recipe
+    @recipe = Recipe.find params[:id]
+  end
+
+  def init_recipe
+    @recipe = Recipe.new params[:recipe]
+  end
+
+  def collect_recipe_ingredients
+    @recipe_ingredients = @recipe.recipe_ingredients.all
+  end
+
+  def new_recipe_ingredient
+    @recipe_ingredient = RecipeIngredient.new
+  end
+
+  def all_ingredients
+    @ingredients = Ingredient.all
+  end
+
+  def all_recipe_ingredients
+    @all_recipe_ingredients = RecipeIngredient.all
+  end
+
+  def usage_list
+    @usage_list = @all_recipe_ingredients.uniq { |i| i.usage }.select { |u| u.usage != nil }
   end
 
 end
