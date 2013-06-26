ad584d807e68d137e2a02406f94f55573543c4d8
dmoore5050 <dustinmoore@gmail.com>
2013-06-26 10:34:54 -0500

    ENV['SECRET_TOKEN'] added

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,9 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Sbonrails::Application.config.secret_token = 'aba09c37435316120529819c7a8edbdfeba6cb8f43dd45858aabd3669913b20c73d7bfa038305f3b76f9d609b20f7b9d79c0ad542c6a04e63c7997f045302925'
+
+if Rails.env.development? or Rails.env.test?
+  Sbonrails::Application.config.secret_token = 'aba09c37435316120529819c7a8edbdfeba6cb8f43dd45858aabd3669913b20c73d7bfa038305f3b76f9d609b20f7b9d79c0ad542c6a04e63c7997f045302925'
+else
+  Sbonrails::Application.config.secret_token = ENV['SECRET_TOKEN']
+end
